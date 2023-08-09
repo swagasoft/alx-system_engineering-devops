5fdd19acc5a53ded767cd23a51a9b6e0c30d2913
Swagasoft <swagasoft@gmail.com>
2023-08-09 13:56:54 -0700

    recure

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"top recursive"
-
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 100}
-    if after:
-        params['after'] = after
-    response = requests.get(url,
-                            headers=headers,
-                            params=params,
-                            allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        after = data['data']['after']
-        posts = data['data']['children']
-        for post in posts:
-            hot_list.append(post['data']['title'])
-        if after:
-            return recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
     else:
-        return None
+        return (None)
